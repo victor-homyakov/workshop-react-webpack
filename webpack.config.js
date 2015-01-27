652a1a6d9c836fa94a999218504ae1549299e307
Victor <vkhomyackov@gmail.com>
2015-01-27 20:01:49 +0300

    Add bootstrap etc.

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,81 @@
 /*
  * Webpack development server configuration
  *
- * This file is set up for serving the webpak-dev-server, which will watch for changes and recompile as required if
+ * This file is set up for serving the webpack-dev-server, which will watch for changes and recompile as required if
  * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
  */
 'use strict';
+
 var webpack = require('webpack');
 
 module.exports = {
 
-  output: {
-    filename: 'main.js',
-    publicPath: '/assets/'
-  },
+    output: {
+        filename: 'main.js',
+        publicPath: '/assets/'
+    },
 
-  cache: true,
-  debug: true,
-  devtool: false,
-  entry: [
-      'webpack/hot/only-dev-server',
-      './src/scripts/components/main.js'
-  ],
+    cache: true,
+    debug: true,
+    devtool: false,
+    entry: [
+        'webpack/hot/only-dev-server',
+        './src/scripts/components/main.js'
+    ],
 
-  stats: {
-    colors: true,
-    reasons: true
-  },
+    stats: {
+        colors: true,
+        reasons: true
+    },
 
-  resolve: {
-    extensions: ['', '.js']
-  },
-  module: {
-    preLoaders: [{
-      test: '\\.js$',
-      exclude: 'node_modules',
-      loader: 'jshint'
-    }],
-    loaders: [{
-      test: /\.js$/,
-      loader: 'react-hot!jsx-loader?harmony'
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.(png|jpg)$/,
-      loader: 'url-loader?limit=8192'
-    }]
-  },
+    resolve: {
+        extensions: ['', '.js']
+    },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    module: {
+        preLoaders: [{
+            test: '\\.js$',
+            exclude: 'node_modules',
+            loader: 'jshint'
+        }],
 
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'react-hot!jsx-loader?harmony'
+            }, {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            },
+
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff2"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
 };
--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -3,55 +3,77 @@
  *
  * This file is set up for serving the distribution version. It will be compiled to dist/ by default
  */
-
 'use strict';
 
 var webpack = require('webpack');
 
 module.exports = {
 
-  output: {
-    publicPath: '/assets/',
-    path: 'dist/assets/',
-    filename: 'main.js'
-  },
+    output: {
+        publicPath: '/assets/',
+        path: 'dist/assets/',
+        filename: 'main.js'
+    },
 
-  debug: false,
-  devtool: false,
-  entry: './src/scripts/components/main.js',
+    debug: false,
+    devtool: false,
+    entry: './src/scripts/components/main.js',
 
-  stats: {
-    colors: true,
-    reasons: false
-  },
+    stats: {
+        colors: true,
+        reasons: false
+    },
 
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin()
-  ],
+    resolve: {
+        extensions: ['', '.js']
+    },
 
-  resolve: {
-    extensions: ['', '.js']
-  },
+    module: {
+        preLoaders: [{
+            test: '\\.js$',
+            exclude: 'node_modules',
+            loader: 'jshint'
+        }],
 
-  module: {
-    preLoaders: [{
-      test: '\\.js$',
-      exclude: 'node_modules',
-      loader: 'jshint'
-    }],
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'jsx-loader?harmony'
+            }, {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            },
 
-    loaders: [{
-      test: /\.js$/,
-      loader: 'jsx-loader?harmony'
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.(png|jpg)$/,
-      loader: 'url-loader?limit=8192'
-    }]
-  }
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff2"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin()
+    ]
 };
